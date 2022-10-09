59cf65764a3a0bc6f24abf6328deced0634ead4a
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-10-09 22:40:27 +0900

    Updated roles

---
+++ roles/rbac/files/convert_rbac_object_to_uuid.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+main() {
+    local object_type="$1"
+    local object_id="$2"
+    local domain="$3"
+    local project_id="$4"
+
+    local object_uuid
+
+    # FIXME: This instruction should be omitted by declaring auth parameters in environment variables.
+    . ./admin-openrc || {
+        echo "ERROR: Failed to load auth information [source ${HOME}/admin-openrc]"
+        return 1
+    }
+
+    if [[ "$object_id" =~ ^[0-9a-zA-Z]{8}\-[0-9a-zA-Z]{4}\-[0-9a-zA-Z]{4}\-[0-9a-zA-Z]{4}\-[0-9a-zA-Z]{12}$ ]]; then
+        echo "$object_id"
+        return 0
+    fi
+
+    if [ ! "$object_type" = "network" ]; then
+        echo "ERROR: This script only support object_type == \"network\" so far. You specified \"object_id == ${object_id}\"" >&2
+        return 1
+    fi
+
+    object_uuid="$(jq -r '.[] | .ID' < <(openstack network list --name "${object_id}" --project-domain "${domain}" --project "${project_id}" --format json))"
+
+    if [[ ! "$object_uuid" =~ ^[0-9a-zA-Z]{8}\-[0-9a-zA-Z]{4}\-[0-9a-zA-Z]{4}\-[0-9a-zA-Z]{4}\-[0-9a-zA-Z]{12}$ ]]; then
+        echo "ERROR: Failed to get object_uuid. [command=\"openstack network list --name "${object_id}" --project-domain "${domain}" --project "${project_id}" --format json\"]" >&2
+        return 1
+    fi
+
+    echo "$object_uuid"
+
+    return 0
+}
+
+main "$@"
